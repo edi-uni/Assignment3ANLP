12a39a6d9b743e6ad180b73538744a9262a472ec
Gabriel Badila <s1879797@okabe.inf.ed.ac.uk>
2018-11-26 13:00:13 +0000

    In proress

--- asgn3_nice.py
+++ asgn3_nice.py
@@ -9,12 +9,14 @@ import numpy as np
 import operator
 import collections
 import csv
+from scipy import stats
 
 STEMMER = PorterStemmer()
 
 # helper function to get the count of a word (string)
 def w_count(word):
 	return o_counts[word2wid[word]]
+
 
 def tw_stemmer(word, stemmed_to_original):
 	'''Stems the word using Porter stemmer, unless it is a
@@ -32,6 +34,7 @@ def tw_stemmer(word, stemmed_to_original):
 		stemmed_word = STEMMER.stem(word)
 		stemmed_to_original[stemmed_word].append(word)
 		return stemmed_word
+
 
 def PMI(c_xy, c_x, c_y, N):
 	'''Compute the pointwise mutual information using cooccurrence counts.
@@ -51,11 +54,13 @@ def PMI(c_xy, c_x, c_y, N):
 	pmi = log((N * c_xy) / (c_x * c_y), 2)
 	return pmi
 
+
 # Do a simple error check using value computed by hand
 if(PMI(2,4,3,12) != 1): # these numbers are from our y,z example
 	print("Warning: PMI is incorrectly defined")
 else:
 	print("PMI check passed")
+
 
 def cos_sim(v0, v1):
 	'''Compute the cosine similarity between two sparse vectors.
@@ -78,6 +83,7 @@ def cos_sim(v0, v1):
 
 	cos = sum_both / (np.sqrt(sum_v0) * np.sqrt(sum_v1))
 	return cos
+
 
 def jaccard(v0, v1):
 	'''Compute the jaccard similarity between two sparse vectors.
@@ -113,6 +119,7 @@ def dice_coefficient(v0, v1):
 	intersection_cardinality = len(set(x) & set(y))
 	return 2 * intersection_cardinality/(len(x) + len(y))
 
+
 def create_ppmi_vectors(wids, o_counts, co_counts, tot_count):
 	'''Creates context vectors for the words in wids, using PPMI.
 	These should be sparse vectors.
@@ -141,6 +148,7 @@ def create_ppmi_vectors(wids, o_counts, co_counts, tot_count):
 		vectors[wid0] = vect
 	return vectors
 
+
 def read_counts(filename, wids):
 	'''Reads the counts from file. It returns counts for all words, but to
 	save memory it only returns cooccurrence counts for the words
@@ -165,6 +173,86 @@ def read_counts(filename, wids):
 			co_counts[wid0] = dict([int(y) for y in x.split(" ")] for x in line[2:])
 	return (o_counts, co_counts, N)
 
+
+# Matches adjectives to WordNet synset form
+adjectives_to_synset = {
+	'happy': 'happiness.n.01',
+	'delighted': 'delight.n.01',
+	'ecstatic': 'ecstasy.n.01',
+	'cheerful': 'cheerfulness.n.01',
+	'calm': 'calm.n.01',
+	'peaceful': 'peace.n.01',
+	'relaxed': 'relaxation.n.03',
+	'quiet': 'quiet.n.04',
+	'serene': 'serenity.n.01',
+	'angry': 'anger.n.01',
+	'irritated': 'irritation.n.01',
+	'enraged': 'rage.n.01',
+	'annoyed': 'annoyance.n.01',
+	'hateful': 'hate.n.01',
+	'sad': 'sadness.n.01',
+	'depressed': 'depression.n.01',
+	'grieved': 'grief.n.01',
+	'unhappy': 'unhappiness.n.01',
+	'upset': 'upset.n.01'
+}
+
+def create_target_similarities(feelings):
+    '''Creates the target similarities dictionaries (arrays) using Path, 
+    Leacock-Chodorow and Wu-Palmer Similarities algorithms
+
+	:type feelings: array of strings
+	:param feelings: all the feelings word used
+	:returns: path_similarities (array and dict), lch_similarities (array and dict),
+                wup_similarities (array and dict)
+	'''
+    
+    path_similarities_dict = {}
+    lch_similarities_dict = {}
+    wup_similarities_dict = {}
+    
+    path_similarities = []
+    lch_similarities = []
+    wup_similarities = []
+    
+    for i, word1 in enumerate(feelings):
+    	for j, word2 in enumerate(feelings):
+    		if i >= j:
+    			continue
+    
+    		wid1 = word2wid[STEMMER.stem(word1)]
+    		wid2 = word2wid[STEMMER.stem(word2)]
+    
+    		if word1 in adjectives_to_synset:
+    			word11 = adjectives_to_synset[word1]
+    		else:
+    			continue
+    
+    		if word2 in adjectives_to_synset:
+    			word22 = adjectives_to_synset[word2]
+    		else:
+    			continue
+    
+    		if word11 != word22:
+    			word11 = wn.synset(word11)
+    			word22 = wn.synset(word22)
+    
+    			sim = word11.path_similarity(word22)
+    			path_similarities.append((word1, word2, sim))
+    			path_similarities_dict[(wid1, wid2)] = sim
+    
+    			sim = word11.lch_similarity(word22)
+    			lch_similarities.append((word1, word2, sim))
+    			lch_similarities_dict[(wid1, wid2)] = sim
+    
+    			sim = word11.wup_similarity(word22)
+    			wup_similarities.append((word1, word2, sim))
+    			wup_similarities_dict[(wid1, wid2)] = sim
+                
+    return path_similarities, path_similarities_dict, lch_similarities,\
+            lch_similarities_dict, wup_similarities, wup_similarities_dict
+
+
 def print_sorted_pairs(similarities, o_counts, first=0, last=100):
 	'''Sorts the pairs of words by their similarity scores and prints
 	out the sorted list from index first to last, along with the
@@ -188,9 +276,32 @@ def print_sorted_pairs(similarities, o_counts, first=0, last=100):
 		print("{:.3f}\t{:30}\t{}\t{}".format(similarities[pair],str(word_pair),
 											o_counts[pair[0]],o_counts[pair[1]]))
 
-def save_to_csv(similarities, file_name):
+
+def print_relation_sentiments_other_words(test_words, word):
+    '''Displays the number of cooccurences between other words (like 'osama' 
+    and 'obama') and the feelings from the input file
+
+	:type test_words: array of strings
+    :type word: sring
+	:param test_words: all the words used for testing: feelings + othe words
+    :param word: a word from the category 'other words'
+	:return: none
+	'''
+    
+    print(word.upper())
+    for test_word in test_words:
+    	if test_word == word:
+    		continue
+    
+    	try:
+    		print(test_word, STEMMER.stem(test_word),co_counts[word2wid[word]][word2wid[STEMMER.stem(test_word)]])
+    	except KeyError:
+    		print(test_word)
+
+
+def save_to_csv(similarities, file_name, first=0, last=100):
 	'''Sorts the pairs of words by their similarity scores and save the sorted
-			list to csv file, along with the counts of each word in each pair.
+	list to csv file, along with the counts of each word in each pair.
 
 	:type similarities: dict
 	:type file_name: string
@@ -206,9 +317,10 @@ def save_to_csv(similarities, file_name):
 		for pair in sorted(similarities.keys(), key=lambda x: similarities[x], reverse = True)[first:last]:
 			writer.writerow([wid2word[pair[0]], wid2word[pair[1]], round(similarities[pair], 3)])
 
+
 def save_target_similarities_to_csv(similarities, file_name):
 	'''Save the target similarities produced using Path, Leacock-Chodorow
-			and Wu-Palmer Similarity algorithms to csv file
+	and Wu-Palmer Similarity algorithms to csv file
 
 	:type similarities: dict
 	:type file_name: string
@@ -216,6 +328,7 @@ def save_target_similarities_to_csv(similarities, file_name):
 	:param last: the name of the csv file which will contain the similarities
 	:return: none
 	'''
+    
 	with open(file_name, 'w') as csvfile:
 		writer = csv.writer(csvfile, delimiter=',',
 							quotechar='|', quoting=csv.QUOTE_MINIMAL)
@@ -223,21 +336,58 @@ def save_target_similarities_to_csv(similarities, file_name):
 		for triple in similarities:
 			writer.writerow([triple[0], triple[1], triple[2]])
 
+
 def freq_v_sim(sims):
-	xs = []
-	ys = []
-	for pair in sims.items():
-		ys.append(pair[1])
-		c0 = o_counts[pair[0][0]]
-		c1 = o_counts[pair[0][1]]
-		xs.append(min(c0,c1))
-	plt.clf() # clear previous plots (if any)
-	plt.xscale('log') #set x axis to log scale. Must do *before* creating plot
-	plt.plot(xs, ys, 'k.') # create the scatter plot
-	plt.xlabel('Min Freq')
-	plt.ylabel('Similarity')
-	print("Freq vs Similarity Spearman correlation = {:.2f}".format(spearmanr(xs,ys)[0]))
-#    plt.show() #display the set of plots
+    '''Plot and compute (using Spearman's rank correlation coefficient) the 
+    relation between similarities and the frequency of words
+
+	:type sims: dict
+	:param sims: the word id pairs (keys) with similarity scores (values)
+	:return: none
+	'''
+    
+    xs = []
+    ys = []
+    for pair in sims.items():
+        ys.append(pair[1])
+        c0 = o_counts[pair[0][0]]
+        c1 = o_counts[pair[0][1]]
+        xs.append(min(c0,c1))
+    plt.clf() # clear previous plots (if any)
+    plt.xscale('log') #set x axis to log scale. Must do *before* creating plot
+    plt.plot(xs, ys, 'k.') # create the scatter plot
+    plt.xlabel('Min Freq')
+    plt.ylabel('Similarity')
+    print("Freq vs Similarity Spearman correlation = {:.2f}".format(stats.spearmanr(xs,ys)[0]))
+    plt.show() #display the set of plots
+   
+    
+def sim_v_sim(sims1, sims2):
+    '''Plot and compute (using Spearman's rank correlation coefficient) the 
+    relation between two types of similarities
+
+	:type sims1: dict
+    :type sims2: dict
+	:param sims1: the word id pairs (keys) with similarity scores (values)
+    :param sims2: the word id pairs (keys) with similarity scores (values)
+	:return: none
+	'''
+    
+    xs = []
+    ys = []
+    for pair in sims1.items():
+        xs.append(pair[1])
+        ys.append(sims2[pair[0]])
+    plt.clf() # clear previous plots (if any)
+    fig = plt.figure(figsize=(5, 5))
+    ax = fig.add_subplot(111)
+    ax.plot(xs, ys, 'k.') # create the scatter plot
+    plt.xlabel('Cosine Similarity')
+    plt.ylabel('Jaccard index')
+    plt.savefig('spearmanr.pdf')
+    print("Similarity 1 vs Similarity 2 Spearman correlation = {:.2f}".format(stats.spearmanr(xs,ys)[0]))
+    plt.show() #display the set of plots
+
 
 def make_pairs(items):
 	'''Takes a list of items and creates a list of the unique pairs
@@ -249,22 +399,22 @@ def make_pairs(items):
 	:return: list of pairs
 
 	'''
+    
 	return [(x, y) for x in items for y in items if x < y]
+
 
 # Test words for preliminary task
 #test_words = ["cat", "dog", "mouse", "computer","@justinbieber"]
 
 test_words = []
-feelings = []
 spamReader = csv.reader(open('feelings.csv', newline=''), delimiter=',', quotechar='|')
 for row in spamReader:
 	row_words = [word for word in row]
-	if row_words[0] not in ['obama', 'osama']:
-		feelings.extend(row_words)
 	test_words.extend(row_words)
 
 
 stemmed_to_original = collections.defaultdict(list)
+stemmed_to_original_large = collections.defaultdict(list)
 stemmed_to_original_all = collections.defaultdict(list)
 
 with open("/afs/inf.ed.ac.uk/group/teaching/anlp/lab8/wid_word") as fp:
@@ -285,124 +435,80 @@ all_wids = set([word2wid[x] for x in stemmed_words]) # stemming might create dup
 wid_pairs = make_pairs(all_wids)
 
 #read in the count information
-(o_counts, co_counts, N) = read_counts("/afs/inf.ed.ac.uk/group/teaching/anlp/asgn3/counts", all_wids)
+#(o_counts, co_counts, N) = read_counts("/afs/inf.ed.ac.uk/group/teaching/anlp/asgn3/counts", all_wids)
 
 #make the word vectors
-vectors = create_ppmi_vectors(all_wids, o_counts, co_counts, N)
+#vectors = create_ppmi_vectors(all_wids, o_counts, co_counts, N)
 
 
 
 
-# Print all cooccurences between 'osama' and the feelings
+test_words_large = test_words + [STEMMER.stem('osama'), STEMMER.stem('obama')]
+stemmed_words_large = [tw_stemmer(w, stemmed_to_original_large) for w in test_words_large]
+#stemmed_words = test_words
+
+all_wids_large = set([word2wid[x] for x in stemmed_words_large]) # stemming might create duplicates; remove them
+
+# you could choose to just select some pairs and add them by hand instead
+# but here we automatically create all pairs
+wid_pairs_large = make_pairs(all_wids_large)
+
+#read in the count information
+(o_counts, co_counts, N) = read_counts("/afs/inf.ed.ac.uk/group/teaching/anlp/asgn3/counts", all_wids_large)
+
+#make the word vectors
+vectors = create_ppmi_vectors(all_wids_large, o_counts, co_counts, N)
+
+
+
+
+
+
+# Print all cooccurences between 'osama' and the feelings ('obama' and feelings)
 print('#'*80)
-print('OSAMA')
-for test_word in test_words:
-	if test_word == 'osama':
-		continue
-	try:
-		print(test_word, STEMMER.stem(test_word),co_counts[word2wid['osama']][word2wid[STEMMER.stem(test_word)]])
-	except KeyError:
-		print(test_word)
-
-
-# Print all cooccurences between 'obama' and the feelings
+print_relation_sentiments_other_words(test_words, 'osama')
 print('#'*80)
-print('OBAMA')
-for test_word in test_words:
-	if test_word == 'obama':
-		continue
-	try:
-		print(test_word, STEMMER.stem(test_word),co_counts[word2wid['obama']][word2wid[STEMMER.stem(test_word)]])
-	except KeyError:
-		print(test_word)
+print_relation_sentiments_other_words(test_words, 'obama')
 print('#'*80)
 
-adjectives_to_synset = {
-	'happy': 'happiness.n.01',
-	'delighted': 'delight.n.01',
-	'ecstatic': 'ecstasy.n.01',
-	'cheerful': 'cheerfulness.n.01',
-	'calm': 'calm.n.01',
-	'peaceful': 'peace.n.01',
-	'relaxed': 'relaxation.n.03',
-	'quiet': 'quiet.n.04',
-	'serene': 'serenity.n.01',
-	'angry': 'anger.n.01',
-	'irritated': 'irritation.n.01',
-	'enraged': 'rage.n.01',
-	'annoyed': 'annoyance.n.01',
-	'hateful': 'hate.n.01',
-	'sad': 'sadness.n.01',
-	'depressed': 'depression.n.01',
-	'grieved': 'grief.n.01',
-	'unhappy': 'unhappiness.n.01',
-	'upset': 'upset.n.01'
-}
 
-nouns_to_adjectives = {v: k for k, v in adjectives_to_synset.items()}
 
-path_similarities_dict = {}
-lch_similarities_dict = {}
-wup_similarities_dict = {}
+            
+path_similarities, path_similarities_dict, lch_similarities,lch_similarities_dict,\
+    wup_similarities, wup_similarities_dict = create_target_similarities(test_words)
+    
+    
+def compute_similarities(vectors, wid_pairs, file_name, sim_func):
+    '''Creates the target similarities dictionaries (arrays) using Path, 
+    Leacock-Chodorow and Wu-Palmer Similarities algorithms
 
-path_similarities = []
-lch_similarities = []
-wup_similarities = []
+	:type vectors: array
+	:type wid_pairs: list of pairs
+	:type file_name: string
+	:type sim_func: function
+	:param vectors: PPMI vectors
+	:param wid_pairs: pairs of wids
+	:param file_name: the name of the output file for similarities
+	:param sim_func: the function applied to compute the similarities
+	:rtype: dict
+	:return: the similarities obtained using sim_func
+	'''
+    
+    print("Compute", file_name)
+    sims = {(wid0,wid1): sim_func(vectors[wid0],vectors[wid1]) for (wid0,wid1) in wid_pairs}
+    print("Sort by", file_name)
+    save_to_csv(sims, file_name=file_name + '.csv', last=-1)
+    
+    return sims
 
-for i, word1 in enumerate(feelings):
-	for j, word2 in enumerate(feelings):
-		if i >= j:
-			continue
 
-		wid1 = word2wid[STEMMER.stem(word1)]
-		wid2 = word2wid[STEMMER.stem(word2)]
+c_sims = compute_similarities(vectors, wid_pairs, 'cos_sim', cos_sim)
+j_sims = compute_similarities(vectors, wid_pairs, 'jaccard', jaccard)
+d_sims = compute_similarities(vectors, wid_pairs, 'dice', dice_coefficient)
 
-		if word1 in adjectives_to_synset:
-			word11 = adjectives_to_synset[word1]
-		else:
-			continue
-
-		if word2 in adjectives_to_synset:
-			word22 = adjectives_to_synset[word2]
-		else:
-			continue
-
-		if word11 != word22:
-			word11 = wn.synset(word11)
-			word22 = wn.synset(word22)
-
-			sim = word11.path_similarity(word22)
-			path_similarities.append((word1, word2, sim))
-			path_similarities_dict[(wid1, wid2)] = sim
-
-			sim = word11.lch_similarity(word22)
-			lch_similarities.append((word1, word2, sim))
-			lch_similarities_dict[(wid1, wid2)] = sim
-
-			sim = word11.wup_similarity(word22)
-			wup_similarities.append((word1, word2, sim))
-			wup_similarities_dict[(wid1, wid2)] = sim
-
-# compute cosine similarities for all pairs we consider and print (save) them
-print("Compute cos_sim")
-c_sims = {(wid0,wid1): cos_sim(vectors[wid0],vectors[wid1]) for (wid0,wid1) in wid_pairs}
-print("Sort by cosine similarity")
-print_sorted_pairs(c_sims, o_counts, last=-1)
-save_to_csv(c_sims, file_name='cos_sim.csv')
-
-# compute jaccard similarities for all pairs we consider and print (save) them
-print("Compute jaccard")
-j_sims = {(wid0,wid1): jaccard(vectors[wid0],vectors[wid1]) for (wid0,wid1) in wid_pairs}
-print("Sort by jaccard similarity")
-print_sorted_pairs(j_sims, o_counts, last=-1)
-save_to_csv(j_sims, file_name='jaccard.csv')
-
-# compute jaccard similarities for all pairs we consider and print (save) them
-print("Compute dice coefficient")
-d_sims = {(wid0,wid1): dice_coefficient(vectors[wid0],vectors[wid1]) for (wid0,wid1) in wid_pairs}
-print("Sort by dice-coefficient similarity")
-print_sorted_pairs(d_sims, o_counts, last=-1)
-save_to_csv(d_sims, file_name='dice.csv')
+c_sims_large = compute_similarities(vectors, wid_pairs_large, 'cos_sim_large', cos_sim)
+j_sims_large = compute_similarities(vectors, wid_pairs_large, 'jaccard_large', jaccard)
+d_sims_large = compute_similarities(vectors, wid_pairs_large, 'dice_large', dice_coefficient)
 
 save_target_similarities_to_csv(path_similarities, 'path_similarities.csv')
 save_target_similarities_to_csv(lch_similarities, 'lch_similarities.csv')
@@ -461,14 +567,12 @@ def compute_error(cos_outputs, jaccard_outputs, targets, error_function, to_norm
 	:return: none
 	'''
 
-	print('*'*80)
 	if to_normalize:
 		targets = normalize(targets)
 
 	cos_error = root_mean_squared_error(cos_outputs, targets)
 	jaccard_error = root_mean_squared_error(jaccard_outputs, targets)
 	print(cos_error, jaccard_error)
-print('#'*80)
 
 def root_mean_squared_error(outputs, targets):
 	'''Compute error using root mean squared error (RMSE)
@@ -486,20 +590,11 @@ def root_mean_squared_error(outputs, targets):
 		suma += (outputs[output_key] - targets[target_key]) ** 2
 	return sqrt(suma / len(targets))
 
+print('#'*80)
 path_similarities_errors = compute_error(c_sims, j_sims, path_similarities_dict_sorted, root_mean_squared_error, True)
+print('*'*80)
 lch_similarities_errors = compute_error(c_sims, j_sims, lch_similarities_dict_sorted, root_mean_squared_error, True)
+print('*'*80)
 wup_similarities_errors = compute_error(c_sims, j_sims, wup_similarities_dict_sorted, root_mean_squared_error, True)
+print('#'*80)
 
-
-'''
-for k, v in adjectives_to_synset.items():
-	word = wn.synset(v)
-	print (v.upper(), word.definition())
-'''
-
-
-for k in adjectives_to_synset.keys():
-	try:
-		co_counts[word2wid['enrag']][word2wid[STEMMER.stem(word)]]
-	except:
-		print (k)
